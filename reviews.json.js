f40866ab3d151cb34a8ba835bafd5488c18254a0
akshayjith <akshayjith@gmail.com>
2025-11-13 22:06:39 +0530

    all files

---
+++ reviews.json.js
@@ -0,0 +1,21 @@
+window.reviewsData = [
+    {
+        "name": "Sara Jain",
+        "source": "Instagram",
+        "review": "Our stay was absolutely perfect! The resort is stunning, the atmosphere relaxing, and every detail was so well thought out. Thank you for making our holiday so special!",
+        "rating": 5
+    },
+    {
+        "name": "Nikhil Joseph",
+        "source": "Booking.com",
+        "review": "Amazing stay! The private pool was fantastic and the location was peaceful.",
+        "rating": 5
+    },
+    {
+        "name": "Archana",
+        "source": "Airbnb",
+        "review": "Absolutely loved staying at Thunder Hill resort in Wayanad. The peaceful surroundings and serene ambiance made it perfect for a relaxing getaway. The entire homestay is spotlessly clean and well-organized.Perfect for families and relaxation seekers. Highly recommended.",
+        "rating": 5
+    },
+   
+]
